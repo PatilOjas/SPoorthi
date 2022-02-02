f0a4ae1fce4126531f46a91d7749c0f11fe33c67
Ojas Patil <ojaspatil1901@gmail.com>
2022-02-03 00:36:01 +0530

    SPoorthi2022 v1.4

--- sheet.py
+++ sheet.py
@@ -1,7 +1,9 @@
 import gspread
+from decouple import config
+
 
 class SheetEditor:
 	def __init__(self,sheetName, sheet) -> None:
-		self.sa = gspread.service_account(filename=GOOGLE_SHEET_CREDENTIALS)
+		self.sa = gspread.service_account_from_dict(eval(config('GOOGLE_SHEET_CREDENTIALS')))
 		self.sh = self.sa.open(sheetName)
 		self.wks = self.sh.worksheet(sheet)
