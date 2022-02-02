0dd3acc1af74b54a87b9667f1756b6f5002e7103
Ojas Patil <ojaspatil1901@gmail.com>
2022-02-03 00:12:56 +0530

    SPoorthi2022 v1.4

--- sheet.py
+++ sheet.py
@@ -2,6 +2,6 @@ import gspread
 
 class SheetEditor:
 	def __init__(self,sheetName, sheet) -> None:
-		self.sa = gspread.service_account(filename="spoorthi-336812-2c50e4c92911.json")
+		self.sa = gspread.service_account(filename=GOOGLE_SHEET_CREDENTIALS)
 		self.sh = self.sa.open(sheetName)
 		self.wks = self.sh.worksheet(sheet)
