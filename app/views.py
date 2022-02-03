835a1141900270423f537011ae561a962886e1fc
Ojas Patil <ojaspatil1901@gmail.com>
2022-02-03 09:45:02 +0530

    SPoorthi2022 v1.6

--- app/views.py
+++ app/views.py
@@ -15,6 +15,7 @@ from sheet import SheetEditor
 def registrationPage(request, name='*'):
 	offset = 2
 	sheet_name = "SPoorthi"
+	message = ""
 	if request.method == 'POST':
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
@@ -23,65 +24,80 @@ def registrationPage(request, name='*'):
 			if saved_data.event == "Cricket":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.The captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/j5TZKuzn7CkAkhQD9\n\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Football":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the appropriate links provided below.\nBoys:https://forms.gle/zm2QpcKxszmuqxPi8 \nGirls: https://forms.gle/6Nx8kPad7R2ExT1H9\n\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Volleyball":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/F341jRPXbHw8gXGh6\n\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Badminton (S)":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Badminton (D)":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:I{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.secondPlayerName, saved_data.secondPlayerMobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you and {saved_data.secondPlayerName} have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Carrom (S)":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Carrom (D)":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:I{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.secondPlayerName, saved_data.secondPlayerMobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you and {saved_data.secondPlayerName} have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Kabaddi":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/PW74hdqqWxdaLxXf6\n\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Chess":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Kho-kho":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/uSXZy1F4MXY8Zmwx9 \n\nShow this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Dodgeball":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\nTeam: https://forms.gle/DxMaFXiMzWrdA578A \n\nShow this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Throwball":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/X1sJb4qhUG7mMKDo6 \n\nShow this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Table Tennis":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 			elif saved_data.event == "Tug of War":
 				sh = SheetEditor(sheetName=sheet_name, sheet=saved_data.event)
 				sh.wks.update(f"A{len(sh.wks.get_all_records()) + offset}:G{len(sh.wks.get_all_records()) + offset}", [[saved_data.playerId, saved_data.email, saved_data.fullName, saved_data.mobNo, saved_data.collegeName, saved_data.event, str(saved_data.datetimestamp)]])
+				message = f"Greetings From Spoorthi SPIT,\n\nHello {saved_data.fullName}, you have successfully registered for {saved_data.event}.\n\nThe captain of the team is supposed to enter the team details in the link provided below.\n\nTeam: https://forms.gle/wRDpQGRRk375nhr78 \n\nShow this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
 			
 
 			subject = "Successfully registered for SPoorthi!"
-			message = f"Greetings From Spoorthi SPIT,\nHello {saved_data.fullName}, you have succesfully registered for {saved_data.event}.\nPlease Show this email at the time of Event.\nSee you at SPoorthi from 15th Feb'22-3rd Mar'22.\nSports Team At SPIT"
-			# from_email = settings.EMAIL_HOST_USER
-			# mail = send_mail(subject, message, from_email, [request.POST['email']], fail_silently=False)
+			from_email = settings.EMAIL_HOST_USER
+			send_mail(subject, message, from_email, [request.POST['email']], fail_silently=False)
+
 			messages.success(request, 'Your have been registerred successfuly!')
+			
 			return redirect('events')
 	
 	return render(request, 'index.html', {'pageTitle':"Register"})
